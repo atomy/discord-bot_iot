f304b684e317a792406be9ae61ccbc7f548ad615
Marcel Lamm <marcel.lamm@jackinpoint.net>
2020-09-22 00:11:44 +0200

    - initial commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@192.168.2.20:~/.docker/config.json
+scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@192.168.2.22:~/.docker/config.json
 
-scp -o StrictHostKeyChecking=no docker-compose.kitchen.yml pi@${ip}:/home/pi/apps/discord-bot_iot_kitchen/docker-compose.yml
+scp -o StrictHostKeyChecking=no docker-compose.kitchen.yml pi@192.168.2.22:/home/pi/apps/discord-bot_iot_kitchen/docker-compose.yml
 #scp -o StrictHostKeyChecking=no docker-compose.buero.yml pi@${ip}:/home/pi/apps/discord-bot_iot_buero/docker-compose.yml
 #scp -o StrictHostKeyChecking=no docker-compose.relay.yml pi@${ip}:/home/pi/apps/discord-bot_iot_relay/docker-compose.yml
 
-ssh -o StrictHostKeyChecking=no pi@192.168.2.20 "cd /home/pi/apps/discord-bot_iot_kitchen && docker-compose pull && docker-compose up -d"
+ssh -o StrictHostKeyChecking=no pi@192.168.2.22 "cd /home/pi/apps/discord-bot_iot_kitchen && docker-compose pull && docker-compose up -d"
 #ssh -o StrictHostKeyChecking=no pi@192.168.2.20 "cd /home/pi/apps/discord-bot_iot_buero && docker-compose pull && docker-compose up -d"
 #ssh -o StrictHostKeyChecking=no pi@192.168.2.20 "cd /home/pi/apps/discord-bot_iot_relay && docker-compose pull && docker-compose up -d"

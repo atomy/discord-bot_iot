f5f195a226f53251e637c9fca6b1c08ddb109d04
Marcel Lamm <marcel.lamm@jackinpoint.net>
2021-03-10 22:25:42 +0100

    - tweak deploy-script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,29 +2,16 @@
 
 set -e
 
-echo "Deploying to pi-kitchen@2003:d4:1f23:ba00:2427:f7a3:3c10:c4ad..."
-scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@2003:d4:1f23:ba00:2427:f7a3:3c10:c4ad:~/.docker/config.json
-scp -o StrictHostKeyChecking=no docker-compose.kitchen.yml pi@2003:d4:1f23:ba00:2427:f7a3:3c10:c4ad:/home/pi/apps/discord-bot_iot/docker-compose.yml
-scp -o StrictHostKeyChecking=no scripts/deployInner.sh pi@2003:d4:1f23:ba00:2427:f7a3:3c10:c4ad:/home/pi/apps/discord-bot_iot/deployInner.sh
-scp -o StrictHostKeyChecking=no scripts/notification.sh pi@2003:d4:1f23:ba00:2427:f7a3:3c10:c4ad:/home/pi/apps/discord-bot_iot/notification.sh
-scp -o StrictHostKeyChecking=no changes pi@2003:d4:1f23:ba00:2427:f7a3:3c10:c4ad:/home/pi/apps/discord-bot_iot/changes
-ssh -o StrictHostKeyChecking=no pi@2003:d4:1f23:ba00:2427:f7a3:3c10:c4ad "cd /home/pi/apps/discord-bot_iot && ./deployInner.sh"
-echo "Deploying to pi-kitchen@2003:d4:1f23:ba00:2427:f7a3:3c10:c4ad...DONE"
+ips=( "2003:d4:1f23:ba00:5ff:4195:12d:ae12" "2003:d4:1f23:ba00:f4d0:9c00:206f:69fa" "2003:d4:1f23:ba00:2427:f7a3:3c10:c4ad" )
 
-echo "Deploying to pi-office@2003:d4:1f23:ba00:f4d0:9c00:206f:69fa..."
-scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@2003:d4:1f23:ba00:f4d0:9c00:206f:69fa:~/.docker/config.json
-scp -o StrictHostKeyChecking=no docker-compose.office.yml pi@2003:d4:1f23:ba00:f4d0:9c00:206f:69fa:/home/pi/apps/discord-bot_iot/docker-compose.yml
-scp -o StrictHostKeyChecking=no scripts/deployInner.sh pi@2003:d4:1f23:ba00:f4d0:9c00:206f:69fa:/home/pi/apps/discord-bot_iot/deployInner.sh
-scp -o StrictHostKeyChecking=no scripts/notification.sh pi@2003:d4:1f23:ba00:f4d0:9c00:206f:69fa:/home/pi/apps/discord-bot_iot/notification.sh
-scp -o StrictHostKeyChecking=no changes pi@2003:d4:1f23:ba00:f4d0:9c00:206f:69fa:/home/pi/apps/discord-bot_iot/changes
-ssh -o StrictHostKeyChecking=no pi@2003:d4:1f23:ba00:f4d0:9c00:206f:69fa "cd /home/pi/apps/discord-bot_iot && ./deployInner.sh"
-echo "Deploying to pi-office@2003:d4:1f23:ba00:f4d0:9c00:206f:69fa...DONE"
-
-echo "Deploying to pi-relay2@2003:d4:1f23:ba00:5ff:4195:12d:ae12..."
-scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@2003:d4:1f23:ba00:5ff:4195:12d:ae12:~/.docker/config.json
-scp -o StrictHostKeyChecking=no docker-compose.relay.yml pi@2003:d4:1f23:ba00:5ff:4195:12d:ae12:/home/pi/apps/discord-bot_iot/docker-compose.yml
-scp -o StrictHostKeyChecking=no scripts/deployInner.sh pi@2003:d4:1f23:ba00:5ff:4195:12d:ae12:/home/pi/apps/discord-bot_iot/deployInner.sh
-scp -o StrictHostKeyChecking=no scripts/notification.sh pi@2003:d4:1f23:ba00:5ff:4195:12d:ae12:/home/pi/apps/discord-bot_iot/notification.sh
-scp -o StrictHostKeyChecking=no changes pi@2003:d4:1f23:ba00:5ff:4195:12d:ae12:/home/pi/apps/discord-bot_iot/changes
-ssh -o StrictHostKeyChecking=no pi@2003:d4:1f23:ba00:5ff:4195:12d:ae12 "cd /home/pi/apps/discord-bot_iot && ./deployInner.sh"
-echo "Deploying to pi-relay2@2003:d4:1f23:ba00:5ff:4195:12d:ae12...DONE"
+for ip in "${ips[@]}"
+do
+  echo "Deploying to pi@${ip}..."
+  scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@[${ip}]:~/.docker/config.json
+  scp -o StrictHostKeyChecking=no docker-compose.kitchen.yml pi@[${ip}]:/home/pi/apps/discord-bot_iot/docker-compose.yml
+  scp -o StrictHostKeyChecking=no scripts/deployInner.sh pi@[${ip}]:/home/pi/apps/discord-bot_iot/deployInner.sh
+  scp -o StrictHostKeyChecking=no scripts/notification.sh pi@[${ip}]:/home/pi/apps/discord-bot_iot/notification.sh
+  scp -o StrictHostKeyChecking=no changes pi@[${ip}]:/home/pi/apps/discord-bot_iot/changes
+  ssh -o StrictHostKeyChecking=no pi@${ip} "cd /home/pi/apps/discord-bot_iot && ./deployInner.sh"
+  echo "Deploying to pi@${ip}...DONE"
+done
